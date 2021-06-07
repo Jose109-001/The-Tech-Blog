6d5e72d17604f6d682376d751c6056ebf64237b2
Jose <josephgaspar725@gmail.com>
2021-06-08 08:12:40 +1000

    update files

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,7 @@
 // basically, using express' next(), if a user isn't loggedIn, redirect
 const withAuth = (req, res, next) => {
     if(!req.session.user_id) {
-        next()
-        //res.redirect('/login');
+        res.redirect('/login');
     } else {
         next();
     }
